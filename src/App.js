f42dc51f63d4d771d91061a3bb213b8cd695b45f
saiyam0211 <saiyamkumar2007@gmail.com>
2024-12-05 17:46:09 +0530

    cards left unresponsive

--- src/App.js
+++ src/App.js
@@ -1,3 +1,115 @@
+// import React from "react";
+// import Logo from "./assets/bad.png";
+// import "./App.css";
+// import Saiyam from "./assets/saiyam.png";
+// import Sonal from "./assets/sonal.png";
+// import Naina from "./assets/naina.png";
+// import Janvi from "./assets/janvi.png";
+// import Ronak from "./assets/ronak.png";
+
+
+
+// const TeamPage = () => {
+//   const teamMembers = [
+//     {
+//       name: "Ronak Jain",
+//       role: "College Relation Manager",
+//       image: Ronak,
+//       style: { left: "21%", bottom: "0px", height: "290px", zIndex: 1 },
+//     },
+//     {
+//       name: "Naina Dugar",
+//       role: "Master of Ceremonies",
+//       image: Naina,
+//       style: { left: "38%", bottom: "0px", height: "340px", zIndex: 2 },
+//     },
+//     {
+//       name: "Saiyam Kumar",
+//       role: "Co-Founder",
+//       image: Saiyam,
+//       style: {
+//         left: "55%",
+//         bottom: "0px",
+//         height: "380px",
+//         zIndex: 3,
+//       },
+//     },
+//     {
+//       name: "Sonal",
+//       role: "Team Lead",
+//       image: Sonal,
+//       style: { left: "72%", bottom: "0px", height: "340px", zIndex: 2 },
+//     },
+//     {
+//       name: "Janvi Yadav",
+//       role: "College Relation Manager",
+//       image: Janvi,
+//       style: { left: "89%", bottom: "0px", height: "290px", zIndex: 1 },
+//     },
+//   ];
+
+//   return (
+//     <div className="text-white  min-h-screen flex flex-col items-center bg-black bg-cover bg-[image:url(./assets/bg-bad.png)]">
+//       {/* Header */}
+//       <div className="relative w-full max-w-7xl text-center pt-6">
+//         <h1 className="font-bold ml-64">
+//           <img
+//             src={Logo}
+//             alt="logo"
+//             className="-mt-16 ml-96"
+//             height={100}
+//             width={300}
+//           />
+//         </h1>
+//         <p className="text-lg -mt-20 ml-60">Powered by Mittal Alliance</p>
+//         <h2 className="text-6xl ml-60 mt-4 anton font-black">BENGALURU TEAM</h2>
+//       </div>
+
+//       {/* Team Section */}
+//       <div className="relative flex justify-center items-center w-full max-w-6xl mt-2">
+//         <div className="relative w-full h-[400px]">
+//           {teamMembers.map((member, index) => (
+//             <div
+//               key={index}
+//               className="absolute w-52 -ml-10 bg-gray-800 text-center rounded-lg shadow-xl overflow-hidden"
+//               style={{
+//                 ...member.style,
+//                 boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)", // Box shadow
+//               }}
+//             >
+//               {/* Image */}
+//               <img
+//                 src={member.image}
+//                 alt={member.name}
+//                 style={{
+//                   backgroundImage: `url(${member.image})`,
+//                   backgroundSize: "contain",
+//                   backgroundPosition: "center",
+//                 }}
+//                 className="w-full"
+//               />
+//               {/* Gradient & Text */}
+//               <div
+//                 className="absolute bottom-0 w-full h-1/3 text-white flex flex-col justify-center items-center"
+//                 style={{
+//                   background:
+//                     "linear-gradient(180deg, transparent, rgba(255, 0, 0, 1))", // Gradient
+//                 }}
+//               >
+//                 <h3 className="text-2xl font-semibold">{member.name}</h3>
+//                 <p className="text-sm">{member.role}</p>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TeamPage;
+
+
 import React from "react";
 import Logo from "./assets/bad.png";
 import "./App.css";
@@ -6,8 +118,6 @@ import Sonal from "./assets/sonal.png";
 import Naina from "./assets/naina.png";
 import Janvi from "./assets/janvi.png";
 import Ronak from "./assets/ronak.png";
-
-
 
 const TeamPage = () => {
   const teamMembers = [
@@ -49,24 +159,26 @@ const TeamPage = () => {
   ];
 
   return (
-    <div className="text-white  min-h-screen flex flex-col items-center bg-black bg-cover bg-[image:url(./assets/bg-bad.png)]">
+    <div className="text-white min-h-screen flex flex-col items-center bg-black bg-cover bg-[image:url(./assets/bg-bad.png)]">
       {/* Header */}
-      <div className="relative w-full max-w-7xl text-center pt-6">
-        <h1 className="font-bold ml-64">
+      <div className="relative w-full max-w-7xl text-center pt-6 px-4">
+        <h1 className="font-bold mx-auto -mt-20 sm:ml-96 md:ml-96 lg:ml-96 xl:ml-96">
           <img
             src={Logo}
             alt="logo"
-            className="-mt-16 ml-96"
+            className="mt-6 mx-auto sm:ml-96 md:ml-64 lg:ml-64 xl:ml-64" // Logo positioning for larger screens
             height={100}
             width={300}
           />
         </h1>
-        <p className="text-lg -mt-20 ml-60">Powered by Mittal Alliance</p>
-        <h2 className="text-6xl ml-60 mt-4 anton font-black">BENGALURU TEAM</h2>
+        <p className="text-lg -mt-20 sm:text-center sm:px-4 lg:ml-72">Powered by Mittal Alliance</p>
+        <h2 className="text-4xl sm:text-5xl sm:text-center sm:px-4 mt-4 anton font-black lg:text-6xl xl:text-6xl lg:ml-60">
+          BENGALURU TEAM
+        </h2>
       </div>
 
       {/* Team Section */}
-      <div className="relative flex justify-center items-center w-full max-w-6xl mt-2">
+      <div className="relative flex justify-center items-center w-full max-w-6xl mt-8 lg:mt-0">
         <div className="relative w-full h-[400px]">
           {teamMembers.map((member, index) => (
             <div
